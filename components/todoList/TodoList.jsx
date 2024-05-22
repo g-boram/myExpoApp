e5acf33f1cd7230d897f3c6b115ec8c75b6ed13d
GOBORAM <109337202+g-boram@users.noreply.github.com>
2024-05-22 22:10:14 +0900

    totoList 공부중

--- components/todoList/TodoList.jsx
+++ components/todoList/TodoList.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { View } from 'react-native'
 import ProjectTitle from '../shared/ProjectTitle'
+import TodoCalendar from './TodoCalendar'
 
 const TodoList = () => {
   return (
     <View>
       <ProjectTitle title='TodoList + Calender' />
+      <TodoCalendar />
     </View>
   )
 }
